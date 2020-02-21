f9ecaf44d0569acb6a92b56365c4eb3d013ea92f
proweb4all <proweb4all@narod.ru>
2020-02-21 15:49:37 +0300

    begin MEN-app

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
+
+const PORT = process.env.PORT || 3000
+const app = express()
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+async function start() {
+  try {
+    await mongoose.connect('mongodb+srv://admin:admin@cluster0-vvqgv.mongodb.net/todos', {
+      useNewUrlParser: true,
+      useFindAndModify: false
+    })
+    app.listen(PORT, () => {
+      console.log(`Server has been started on ${PORT}...`)
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
